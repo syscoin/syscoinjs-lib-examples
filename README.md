dd00032e552acc64b1114b3347a9de60b0db5b0f
sidhujag <jsidhu@blockchainfoundry.co>
2020-09-12 21:05:13 -0700

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# SyscoinJS Examples (syscoinjs-lib-examples)
+
+Examples of a javascript Syscoin library for node.js and browsers.
+
+Released under the terms of the [MIT LICENSE](LICENSE).
+
+## Complementing Libraries
+- [bitcoinjs-lib](https://github.com/bitcoinjs/bitcoinjs-lib) - A javascript Bitcoin library for node.js and browsers. Configurable with Syscoin network settings to work with Syscoin addresses and message signing.
+- [BIP84](https://github.com/Anderson-Juhasc/bip84) - P2WPKH/P2WSH HD wallet derivation library for BECH32 addresses
+- [syscointx-js](https://github.com/syscoin/syscointx-js) - A raw transaction creation library using coinselectsyscoin for input selection.
+- [syscoinjs-lib](https://github.com/syscoin/syscoinjs-lib) - High level SDK library for Syscoin in Javascript.
+- [coinselectsyscoin](https://github.com/syscoin/coinselectsyscoin) - A fee-optimizing, transaction input selection module for syscoinjs-tx.
+- [crypto-js](https://github.com/brix/crypto-js) - JavaScript library of crypto standards. Used for AES Encrypt/Decrypt of sensitive HD wallet info to local storage.
+- [axios](https://github.com/axios/axios) - Promise based HTTP client for the browser and node.js. Used for backend communication with a Blockbook API as well as notary endpoints where applicable.
+
+
+
+
+
+## LICENSE [MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,27 @@
+const sjs = require('syscoinjs-lib')
+const BN = require('bn.js')
+const mnemonic = 'club toss element melody skin ship rifle student reason real interest insane elevator beauty movie'
+// blockbook URL
+const backendURL = 'localhost:9130'
+// 'null' for no password encryption for local storage and 'true' for testnet
+const HDSigner = new sjs.utils.HDSigner(mnemonic, null, true)
+const syscoinjs = new sjs.SyscoinJSLib(HDSigner, backendURL)
+console.log('account xpub ' + HDSigner.getAccountXpub())
+async function newAsset () {
+  const feeRate = new BN(10)
+  const txOpts = { rbf: false }
+  const assetOpts = { precision: 8, symbol: 'CAT', updatecapabilityflags: 255, balance: new BN(10000000000), maxsupply: new BN(100000000000), description: 'publicvalue' }
+  // let HDSigner find change address
+  const sysChangeAddress = null
+  const psbt = await syscoinjs.assetNew(assetOpts, txOpts, sysChangeAddress, feeRate)
+  // example of once you have it signed you can push it to network via backend provider
+  const resSend = await sjs.utils.sendRawTransaction(syscoinjs.blockbookURL, psbt.extractTransaction().toHex())
+  if (resSend.error) {
+    console.log('could not send tx! error: ' + resSend.error.message)
+  } else if (resSend.result) {
+    console.log('tx successfully sent! txid: ' + resSend.result)
+  } else {
+    console.log('Unrecognized response from backend')
+  }
+}
+newAsset()
