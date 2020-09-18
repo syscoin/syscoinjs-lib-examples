cadda58fa39987b4eb277f31936d7720659d4b39
sidhujag <jsidhu@blockchainfoundry.co>
2020-09-18 15:41:38 -0700

    add sysBurnToAsset

--- index.js
+++ index.js
@@ -209,4 +209,35 @@ async function assetMintToSys () {
   }
 }
 
-assetMintToSys()
+async function sysBurnToAsset () {
+  const feeRate = new BN(10)
+  const txOpts = { rbf: true } 
+  // asset and address being minted to from Eth to Sys
+  const mintAddress = 'tsys1qdflre2yd37qtpqe2ykuhwandlhq04r2td2t9ae'
+  const assetGuid = 2615707979
+  // mint 100 COINS
+  const amountToMint = new BN(10000000000)
+  
+  // note no destination address in first output as syscoinjslib will auto fill it with new change address for 0 value asset outputs
+  const assetMap = new Map([
+    [assetGuid, { outputs: [{ value: amountToMint, address: mintAddress }] }]
+  ])
+  // let HDSigner find change address
+  const sysChangeAddress = null
+  const psbt = await syscoinjs.syscoinBurnToAssetAllocation(txOpts, assetMap, sysChangeAddress, amountToMint, feeRate)
+  if(!psbt) {
+    console.log('Could not create transaction, not enough funds?')
+    return
+  }
+  // example of once you have it signed you can push it to network via backend provider
+  const resSend = await sjs.utils.sendRawTransaction(syscoinjs.blockbookURL, psbt.extractTransaction().toHex(), HDSigner)
+  if (resSend.error) {
+    console.log('could not send tx! error: ' + resSend.error.message)
+  } else if (resSend.result) {
+    console.log('tx successfully sent! txid: ' + resSend.result)
+  } else {
+    console.log('Unrecognized response from backend')
+  }
+}
+
+sysBurnToAsset()
