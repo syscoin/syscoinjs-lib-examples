ce2dad29d745690e798417f6ead2d74cc12e2cfe
sidhujag <jsidhu@blockchainfoundry.co>
2020-09-21 17:26:21 -0700

    update examples for new and update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const syscoinjs = new sjs.SyscoinJSLib(HDSigner, backendURL)
 async function newAsset () {
   const feeRate = new BN(10)
   const txOpts = { rbf: false }
-  const assetOpts = { precision: 8, symbol: 'CAT', updatecapabilityflags: 255, maxsupply: new BN(100000000000), description: 'publicvalue' }
+  const assetOpts = { precision: 8, symbol: 'CAT', maxsupply: new BN(100000000000), description: 'publicvalue' }
   // let HDSigner find change address
   const sysChangeAddress = null
   // let HDSigner find asset destination address
@@ -36,7 +36,7 @@ async function updateAsset () {
   const txOpts = { rbf: true }
   const assetGuid = 3372068234
   // update capability flags, update description and update eth smart contract address
-  const assetOpts =  { updatecapabilityflags: 127, contract: Buffer.from('2b1e58b979e4b2d72d8bca5bb4646ccc032ddbfc', 'hex'), description: 'new publicvalue' }
+  const assetOpts =  { updatecapabilityflags: 123, contract: Buffer.from('2b1e58b979e4b2d72d8bca5bb4646ccc032ddbfc', 'hex'), description: 'new publicvalue' }
   // let HDSigner find change address
   const sysChangeAddress = null
   const psbt = await syscoinjs.assetUpdate(assetGuid, assetOpts, txOpts, sysChangeAddress, feeRate)
