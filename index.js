a7bf215279d3f6568dcd17c429d41a35a466f803
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-02-25 15:19:45 -0800

    add notary example

--- index.js
+++ index.js
@@ -113,6 +113,42 @@ async function updateAssetAuxFees () {
   }
 }
 
+async function updateAssetNotary () {
+  const feeRate = new sjs.utils.BN(10)
+  const txOpts = { rbf: true }
+  const assetGuid = '2039276165'
+  // the notary keyPair
+  const keyPair = HDSigner.createKeypair(0)
+  // get the p2wpkh payment so we can get the keyid (payment.hash)
+  const payment = sjs.utils.bitcoinjs.payments.p2wpkh({
+    pubkey: keyPair.publicKey,
+    network: HDSigner.network
+  })
+  // the notary key and the details can be updated independently but we will update both here
+  const notaryKeyID = Buffer.from(payment.hash.toString('hex'), 'hex')
+  const notaryDetails =
+  {
+    // endpoint must be encoded in utf8 using Buffer.from (default) because it should encode as bytes represented by base64 string (encodeToBase64)
+    endpoint: Buffer.from(syscointx.utils.encodeToBase64('https://test12.com')),
+    instanttransfers: true,
+    hdrequired: false
+  }
+  // update auxfee details
+  const assetOpts = { notarykeyid: notaryKeyID,  notarydetails: notaryDetails }
+  // send asset back to ourselves as well as any change
+  const assetChangeAddress = null
+  // send change back to ourselves as well as recipient to ourselves
+  const assetMap = new Map([
+    [assetGuid, { changeAddress: assetChangeAddress, outputs: [{ value: new sjs.utils.BN(0), address: assetChangeAddress }] }]
+  ])
+  // if SYS need change sent, set this address. null to let HDSigner find a new address for you
+  const sysChangeAddress = null
+  const psbt = await syscoinjs.assetUpdate(assetGuid, assetOpts, txOpts, assetMap, sysChangeAddress, feeRate)
+  if (!psbt) {
+    console.log('Could not create transaction, not enough funds?')
+  }
+}
+
 async function transferAsset () {
   const feeRate = new sjs.utils.BN(10)
   const txOpts = { rbf: true }
@@ -342,4 +378,4 @@ async function assetMintToSys2 () {
   }
 }
 console.log('Account XPUB: ' + HDSigner.getAccountXpub())
-updateAssetAuxFees()
+newAsset()
