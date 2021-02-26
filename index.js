a482f0a13a2f11132803a8e056e6509597e5e6e2
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-02-25 18:54:19 -0800

    send memo field along with assetAllocationSend

--- index.js
+++ index.js
@@ -264,6 +264,26 @@ async function sendAssetFundedByXPUB () {
   await syscoinjs.signAndSendWithHDSigner(result.res, HDSigner, result.assets)
 }
 
+async function sendAssetWithMemo () {
+  const feeRate = new sjs.utils.BN(10)
+  // data carrying memo field added to opreturn commitment
+  const memo = Buffer.from('a7bf215279d3f6568dcd17c429d41a35a466f803', 'hex')
+  // set to false for ZDAG, true disables it but it is replaceable by bumping the fee
+  const txOpts = { rbf: true, memo: memo }
+  const assetguid = '855502674'
+  // if assets need change sent, set this address. null to let HDSigner find a new address for you
+  const assetChangeAddress = null
+  const assetMap = new Map([
+    [assetguid, { changeAddress: assetChangeAddress, outputs: [{ value: new sjs.utils.BN(5), address: 'tsys1qk0mrytgd06tc4rdtcs7h6nvx9ph67rjavv7qx6' }] }]
+  ])
+  // if SYS need change sent, set this address. null to let HDSigner find a new address for you
+  const sysChangeAddress = null
+  const psbt = await syscoinjs.assetAllocationSend(txOpts, assetMap, sysChangeAddress, feeRate)
+  if (!psbt) {
+    console.log('Could not create transaction, not enough funds?')
+  }
+}
+
 async function assetBurnToSys () {
   const feeRate = new sjs.utils.BN(10)
   const txOpts = { rbf: true }
