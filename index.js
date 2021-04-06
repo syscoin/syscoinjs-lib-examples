f8273ebf302eb95744f82869858b25f295ccd087
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-04-06 13:58:06 -0700

    update sys sending with asset whitelist

--- index.js
+++ index.js
@@ -22,24 +22,25 @@ async function sendSys () {
 }
 async function sendSysMemo () {
   const feeRate = new sjs.utils.BN(10)
-  const memo = Buffer.from('my memo')
+  const memo = Buffer.from('Gas station order id: 606c80d6cc793f13d2f0d424. Asset GUID 341906151')
   const memoHeader = Buffer.from([0xfe, 0xfe, 0xaf, 0xaf, 0xaf, 0xaf])
-  const txOpts = { rbf: true, memo: memo, memoHeader: memoHeader, allowOtherNotarizedAssetInputs: true }
+  const txOpts = { rbf: true, memo: memo, memoHeader: memoHeader, assetWhiteList: new Map([['341906151', {}]]) }
   // if SYS need change sent, set this address. null to let HDSigner find a new address for you
-  const sysChangeAddress = null
+  const sysChangeAddress = 'tsys1qnykk8e2e6wuufaz0lcdlry05fq5sj3uzczra3p'
   const outputsArr = [
-    { address: 'tsys1quas925cuqmlkc0082442uh68efv3hdazknmjpd', value: new sjs.utils.BN(100000000) }
+    { address: 'tsys1q0hxdj9r3tyxmr66tehdhhl35l5rk4ucanteuh6', value: new sjs.utils.BN(3158692) }
   ]
-  const psbt = await syscoinjs.createTransaction(txOpts, sysChangeAddress, outputsArr, feeRate)
-  if (!psbt) {
+  const result = await syscoinjs.createTransaction(txOpts, sysChangeAddress, outputsArr, feeRate, 'tsys1qnykk8e2e6wuufaz0lcdlry05fq5sj3uzczra3p')
+  if (!result) {
     console.log('Could not create transaction, not enough funds?')
   }
+  const psbt = await syscoinjs.signAndSendWithWIF(result.res, 'cQh85hzaUMQDCneQvuwEvKFS418gzHfaQ9MmKj9pCd8aKgRNaEDr', result.assets)
   const memoExtracted = sjs.utils.getMemoFromOpReturn(psbt.txOutputs, memoHeader)
   console.log('memo extracted from transaction ' + memoExtracted.toString())
 }
 async function newAsset () {
   const feeRate = new sjs.utils.BN(10)
-  const txOpts = { rbf: false, allowOtherNotarizedAssetInputs: true }
+  const txOpts = { rbf: false }
   const assetOpts = { precision: 8, symbol: 'JAG', maxsupply: new sjs.utils.BN(100000000000), description: 'publicvalue' }
   // if SYS need change sent, set this address. null to let HDSigner find a new address for you
   const sysChangeAddress = null
